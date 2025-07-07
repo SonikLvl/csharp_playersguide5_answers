c5b440099b536e524e6a5e3c3eb4640eda4d2397
Sonik Kulichenko <sofiy.kulichenko@gmail.com>
2025-07-07 23:10:50 +0300

    Add files via upload

---
+++ LargePrograms/McDroid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McDroid
+{
+    public class Pig
+    {
+    }
+    public class Cow { }
+}
---
+++ LargePrograms/Program.cs
@@ -0,0 +1,17 @@
+﻿using IField;
+using McDroid;
+namespace LargeProgram
+{
+    internal class Program
+    {
+        static void Main(string[] arg)
+        {
+            Cow cow = new Cow();
+            Sheep sheep = new Sheep();
+
+            IField.Pig Ipig = new IField.Pig();
+            McDroid.Pig Mpig = new McDroid.Pig();
+
+        }
+    }
+}
